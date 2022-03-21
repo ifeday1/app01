0c25734eababa092118ea7f86b83695205c9c09b
ifeoluwa oladeni <oladeniifeoluwa123@gmail.com>
2022-03-21 12:24:42 +0100

    frame2

--- src/components/Accounts.jsx
+++ src/components/Accounts.jsx
@@ -19,6 +19,24 @@ const Accounts = () =>{
         <div className="line1"></div>
         <div className="line2"></div>
         <div className="addnow">Add Now</div>
+        <div className="box2"></div>
+        <div className="main-wallet"> Main wallet</div>
+        <div className="num1">$2049.95</div>
+        <div className="prog"></div>
+        <div className="per">+32%</div>
+        <div className="prog1"></div>
+        <div className="box3"></div>
+        <div className="saving-wallet"> Saving Wallet</div>
+        <div className="num2">$65.14 </div>
+        <div className="per1">-0.4%</div>
+        <div className="line3"></div>
+        <div className="line4"></div>
+        <div className="box4"></div>
+        <div className="summer-wallet">Summer Wallet</div>
+        <div className="per3">$547.56</div>
+        <div className="per2">+2.6%</div>
+        <div className="line5"></div>
+        <div className="line6"></div>
         </>
         
     )
