36d55ed01fba8c5b8d2b182a6440c905030c3cb6
ifeoluwa oladeni <oladeniifeoluwa123@gmail.com>
2022-03-21 10:52:29 +0100

    adding frame1

--- src/components/Accounts.jsx
+++ src/components/Accounts.jsx
@@ -1,12 +1,26 @@
 import React from "react";
 import "./Accounts.css"
 
+//importing material ui
+import ArrowBackIosIcon from "@mui/icons-material/ArrowBackIos"
+
 const Accounts = () =>{
 
     return(
-        <div className="frame">
-
-        </div>
+        <>
+            <div className="frame"></div>
+        <div className="account">Accounts</div>
+        <div className="box1"></div>
+        <div> <ArrowBackIosIcon className="arrow-back"/> </div>
+        <div className="box1"></div>
+        <div className="pro-box"></div>
+        <div className="pro">PRO</div>
+        <div className="upgrade">Upgrade your account</div>
+        <div className="line1"></div>
+        <div className="line2"></div>
+        <div className="addnow">Add Now</div>
+        </>
+        
     )
 }
 
